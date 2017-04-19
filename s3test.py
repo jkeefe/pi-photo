d0405ce1d3e03e0a0add9802370eacb9dc100844
John Keefe (pi) <john@johnkeefe.net>
2017-04-19 14:18:25 -0400

    got s3 working

--- s3test.py
+++ s3test.py
@@ -1,14 +1,10 @@
 import boto3
 
 # Let's use Amazon S3
-s3 = boto3.resource('s3')
-
-# Print out bucket names
-for bucket in s3.buckets.all():
-    print(bucket.name)
+s3 = boto3.client('s3')
 
 s3.upload_file(
-    './data/vision_test.json', 'media.johnkeefe.net', 'default',
+    './data/vision_test.json', 'media.johnkeefe.net', 'vision.json',
     ExtraArgs={'ACL': 'public-read'}
 )
 
