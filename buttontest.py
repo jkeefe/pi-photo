1bdf07c030758ead9acdeaefbd9b1362c446b6d7
John Keefe <john@johnkeefe.net>
2017-04-20 12:55:24 -0400

    trying differeng gpio

--- buttontest.py
+++ buttontest.py
@@ -5,7 +5,7 @@
 import time
 import RPi.GPIO as GPIO
 
-LED_GPIO_PIN = 22
+LED_GPIO_PIN = 24
 BUTTON_GPIO_PIN = 23
 
 
@@ -14,12 +14,12 @@ def buttonEventHandler (pin):
     print "handling button event"
 
     # turn the green LED on
-    GPIO.output(LED_GPIO_PIN,True)
+    GPIO.output(LED_GPIO_PIN, GPIO.HIGH)
 
     time.sleep(1)
 
     # turn the green LED off
-    GPIO.output(LED_GPIO_PIN,False)
+    GPIO.output(LED_GPIO_PIN, GPIO.HIGH)
 
 
 
@@ -32,8 +32,8 @@ def main():
 
     # setup pin 23 as an input
     # and set up pins 24 and LED_GPIO_PIN as outputs
-    GPIO.setup(BUTTON_GPIO_PIN,GPIO.IN)
-    GPIO.setup(LED_GPIO_PIN,GPIO.OUT)
+    GPIO.setup(BUTTON_GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(LED_GPIO_PIN, GPIO.OUT)
 
     # tell the GPIO library to look out for an 
     # event on pin BUTTON_GPIO_PIN and deal with it by calling 
