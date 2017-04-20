17a40de7f979da7456b98cae0a137060b634b75a
John Keefe <john@johnkeefe.net>
2017-04-20 13:37:42 -0400

    only fire when rising

--- buttontest.py
+++ buttontest.py
@@ -41,7 +41,7 @@ def main():
     # tell the GPIO library to look out for an 
     # event on pin BUTTON_GPIO_PIN and deal with it by calling 
     # the buttonEventHandler function
-    GPIO.add_event_detect(BUTTON_GPIO_PIN, GPIO.BOTH, buttonEventHandler)
+    GPIO.add_event_detect(BUTTON_GPIO_PIN, GPIO.RISING, buttonEventHandler)
 
     # turn off LEDs
     GPIO.output(GREEN_LED, GPIO.LOW)
